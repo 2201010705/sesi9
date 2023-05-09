e867dce15a631c41a3d1e021af6a72acef328f58
Lodovikus Aldo Charlo <127809435+2201010705@users.noreply.github.com>
2023-05-09 12:11:38 +0800

    Add files via upload

---
+++ sesi9/javascript04.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan 4</title>
+</head>
+<body>
+    
+    <button onclik="Tulisan()"> Tampilkan text</button>
+
+      <div>
+        <from>
+           <div>
+            Nama Lengkap
+            <input type="text" name="txNAMA" id="txNAMA">
+            </dive>
+            <div>
+                <button type="button" id="check" oncliCk="checkNama()">check Nama</button>
+           </div>
+
+</from> 
+</div>
+
+
+
+    
+   <script src="javascript04.js"></script>
+
+
+
+</body>
+</html>
